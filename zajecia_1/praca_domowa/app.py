50d5f3cb4967d596920bc17e911139fe3ca42228
sloniewski <sloniewski.maciej@gmail.com>
2018-03-11 19:21:42 +0100

    praca domowa

--- zajecia_1/praca_domowa/app.py
+++ zajecia_1/praca_domowa/app.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from datetime import datetime
+
+from flask import (
+    Flask,
+    request,
+)
+
+from user_agents import parse
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def time():
-    return 'it works'
+def hello():
+    return "Hello, World!"
 
+
+@app.route('/now')
+def time():
+    now_object = datetime.now()
+    return now_object.isoformat(sep=' ')
+
+
+@app.route('/user-agent')
+def user_agent():
+    ua_string = request.headers['User-Agent']
+    user_agent = parse(ua_string)
+    return '{} / {} / {}'.format(
+        user_agent.device.family,
+        user_agent.os.family,
+        user_agent.browser.family,
+    )
 
 if __name__ == '__main__':
-app.run()
+    app.run()
