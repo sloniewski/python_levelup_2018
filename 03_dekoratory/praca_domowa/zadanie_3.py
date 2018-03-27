b5afb1ee70be476014fcc6411bb5393fbb795b50
sloniewski <sloniewski.maciej@gmail.com>
2018-03-27 22:25:25 +0200

    gotowe zadanie 3

--- 03_dekoratory/praca_domowa/zadanie_3.py
+++ 03_dekoratory/praca_domowa/zadanie_3.py
@@ -2,20 +2,37 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(message)s')
+file_handler = logging.FileHandler('test.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 def log_this(logger, level, format):
     def decorator(function):
         def wrapper(*args, **kwargs):
+            # call function
             result = function(*args, **kwargs)
-            logger.info(format, function.__name__, (args, kwargs), result)
-            print(format, function.__name__, (args, kwargs), result)
+
+            # build message
+            message = format % (function.__name__, (args, kwargs), result)
+
+            # log results with function matching log level
+            logging_func = {
+                10: logger.debug,
+                20: logger.info,
+                30: logger.warning,
+                40: logger.error,
+                50: logger.critical,
+            }
+            logging_func[level](msg=message)
+
             return result
         return wrapper
     return decorator
 
 
-@log_this(logger, level=logging.INFO, format='%s: %s -> %s')
+@log_this(logger=logger, level=logging.DEBUG, format='%s: %s -> %s')
 def my_func(a, b, c=None, d=False):
     return 'Wow!'
 
