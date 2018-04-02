889b93753cce1a289d974fc2327f3f5034cf1ce0
sloniewski <sloniewski.maciej@gmail.com>
2018-04-02 18:04:53 +0200

    simiplified sql

--- 04_sqlite/praca_domowa/zadanie_6.sql
+++ 04_sqlite/praca_domowa/zadanie_6.sql
@@ -3,11 +3,9 @@ ADD COLUMN category_id int
 REFERENCES category(category_id)
 DEFAULT NULL;
 
-WITH a as (
-	SELECT film_id, category_id
-	FROM film_category
-)
 UPDATE film 
 SET category_id = (
-	SELECT category_id FROM a WHERE film_id = film.film_id
+	SELECT category_id FROM film_category WHERE film_id = film.film_id
 );
+
+DROP TABLE film_category;
