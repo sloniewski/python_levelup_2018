1a9950c47c2e3ae0ca72670569e3d64135e5bc63
sloniewski <sloniewski.maciej@gmail.com>
2018-04-02 17:59:03 +0200

    finished SQL for update

--- 04_sqlite/praca_domowa/zadanie_6.sql
+++ 04_sqlite/praca_domowa/zadanie_6.sql
@@ -1,3 +1,13 @@
-SELECT film.film_id, category.name from film 
-JOIN film_category ON film_category.film_id = film.film_id
-JOIN category ON category.category_id = film_category.category_id;
+ALTER TABLE film
+ADD COLUMN category_id int
+REFERENCES category(category_id)
+DEFAULT NULL;
+
+WITH a as (
+	SELECT film_id, category_id
+	FROM film_category
+)
+UPDATE film 
+SET category_id = (
+	SELECT category_id FROM a WHERE film_id = film.film_id
+);
